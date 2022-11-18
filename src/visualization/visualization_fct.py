25535bd696c48ef907f83d38d246b7d85f792945
LaurineDargaud <dargaud.laurine@gmail.com>
2022-11-18 23:48:22 +0100

    adding class names in viz src file

--- src/visualization/visualization_fct.py
+++ src/visualization/visualization_fct.py
@@ -1,14 +1,32 @@
 _MASK_COLORS_ ={
-    0:'#141414', #black
-    1:'#e31b1b', #red
-    2:'#d46408', #orange
-    3:'#f2cf07', #yellow
-    4:'#1cd113', #green
-    5:'#07d9af', #turquoise
-    6:'#0e1ded', #blue
-    7:'#9816d9', #purple
-    8:'#d60bae', #pink
+    0:'#141414', #black #background
+    1:'#e31b1b', #red #front_door
+    2:'#d46408', #orange #back_door
+    3:'#f2cf07', #yellow #front_side
+    4:'#1cd113', #green #back_side
+    5:'#07d9af', #turquoise #front_bumper 
+    6:'#0e1ded', #blue #bonnet #capot
+    7:'#9816d9', #purple #back_bumper
+    8:'#d60bae', #pink #trunk #coffre
 }
+
+_MASK_NAMES_ ={
+    0:'background', #black #background
+    1:'front_door', #red #front_door
+    2:'back_door', #orange #back_door
+    3:'front_side', #yellow #front_side
+    4:'back_side', #green #back_side
+    5:'front_bumper ', #turquoise #front_bumper 
+    6:'bonnet', #blue #bonnet #capot
+    7:'back_bumper', #purple #back_bumper
+    8:'trunk', #pink #trunk #coffre
+}
+
+def get_mask_names():
+    return _MASK_NAMES_
+
+def get_mask_colors():
+    return _MASK_COLORS_
 
 import numpy as np
 
